d01bcbde160e959167e78ed825c7883e4190753e
HaKhanhDuygcs220287 <duyhkgcs220287@fpt.edu.vn>
2025-08-01 16:52:32 +0700

    🚀 Upload toàn bộ source code lên GitHub

--- server/models/vocabModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const vocabSchema = new mongoose.Schema({
-  word: { type: String, required: true },
-  definition: { type: String, required: true },
-  language: { type: String }
-});
-
-module.exports = mongoose.model('Vocab', vocabSchema);
